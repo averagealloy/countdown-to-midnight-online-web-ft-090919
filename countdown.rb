b4c6de6ddd521f8b72573a8693a82a69a72476af
Mike McTighe <m.mctighe10@gmail.com>
2019-09-10 18:39:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ def countdown(number )
   end
   return "HAPPY NEW YEAR!"
 end
-def countdown_with_sleep(number = +10)
+def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECONDS(S)!"
     number -= 1
