c4030170e867faee2162447e8f18dac9520c25ad
Mike McTighe <m.mctighe10@gmail.com>
2019-09-10 18:38:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(number = +10 )
+  while number > 0
+    puts "#{number} SECONDS(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(number = +10)
+  while number > 0
+    puts "#{number} SECONDS(S)!"
+    number -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
